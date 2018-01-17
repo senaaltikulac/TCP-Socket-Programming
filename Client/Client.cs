8397d4e863360ffcd18938af27fc40a0120efdeb
senaaltikulac <senaaltikulac@gmail.com>
2018-01-18 01:15:11 +0300

    Initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        public static string veri = null;
+
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sender.Connect(ipend);
+                Console.WriteLine("Socket Connection to {0}", sender.RemoteEndPoint.ToString());
+
+                while(true)
+                {
+                    Console.Write("Client-me:");
+                    string str = Console.ReadLine();
+                    byte[] msg = Encoding.ASCII.GetBytes(str);
+                    int byteSend = sender.Send(msg);
+                    int byteRec = sender.Receive(bytes);
+                    if (str.IndexOf("<eof>") > -1)
+                    {
+                        break;
+                    }
+                    veri = null;
+                    veri += Encoding.ASCII.GetString(bytes, 0, byteRec);
+                    Console.WriteLine("Server-you :{0}", veri);
+                }
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+
+
+               
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        public static string veri = null;
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(ipend);
+                listener.Listen(10);
+                while(true)
+                {
+                    Console.WriteLine("Waiting for connection !!");
+                    Socket handler = listener.Accept();
+                    veri = null;
+                    while(true)
+                    {
+                        veri = null;
+                        bytes = new byte[1024];
+                        int byteRec = handler.Receive(bytes);
+                        veri += Encoding.ASCII.GetString(bytes, 0, byteRec);
+                        Console.WriteLine("Client- You :{0}", veri);
+                        if(veri.IndexOf("<eof>")> -1)
+                        {
+                            break;
+                        }
+                        veri = null;
+                        Console.Write("Server- Me :");
+                        veri = Console.ReadLine();
+                        byte[] msg = Encoding.ASCII.GetBytes(veri);
+                        handler.Send(msg);
+                    }
+                   
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+
+
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
